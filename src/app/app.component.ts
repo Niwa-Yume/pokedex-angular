aa1171db00ce25c48809254048624fa3880d7d28
Julien Castro <julien.castro@protonmail.com>
2024-08-18 13:31:14 +0200

    ajout de la liste pokémoin + affichage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, signal, computed } from '@angular/core'
+import { POKEMON_LIST } from './pokemon-list.fake';
 
 @Component({
   selector: 'app-root',
@@ -10,31 +11,7 @@ import { Component, signal, computed } from '@angular/core'
 export class AppComponent {
 
   //les propriétés/attributs
-  name = signal('Pikachu');
-  life = signal(21);
-  
-  imageSrc = signal('https://assets.pokemon.com/assets/cms2/img/pokedex/detail/025.png');
-  
-  
-  size = computed(() => {  //computed est une fonction qui permet de calculer une valeur en fonction d'autres valeurs
+  listPokemon = signal(POKEMON_LIST);
 
-    //les conditions
-    if (this.life() <= 15) {
-      return 'Petit';
-    }
 
-    if (this.life() >= 25) {
-      return 'Grand';
-    }
-
-    return 'Moyen';
-  });
-
-  incrementLife() {
-    this.life.update((n) => n + 1);
-  }
-
-  decrementLife() {
-    this.life.update((n) => n - 1);
-  }
 }
