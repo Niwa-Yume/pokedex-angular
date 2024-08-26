6cb2c946c8eb9399a06eebdf6aa9e7fef0849da4
Julien Castro <julien.castro@protonmail.com>
2024-08-26 16:03:14 +0200

    ajout des liens de navigation sur le header e

--- src/app/pokemon/pokemon-profile/pokemon-profile.component.ts
+++ src/app/pokemon/pokemon-profile/pokemon-profile.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PokemonService } from '../../pokemon.service';//C'est le service qui permet de récupérer les données des Pokémons
 
 @Component({
   selector: 'app-pokemon-profile',
-  standalone: true,
-  imports: [],
   templateUrl: './pokemon-profile.component.html',
-  styles: ``
+  styles: ``,
+  standalone: true,
 })
 export class PokemonProfileComponent {
-
+  readonly route = inject(ActivatedRoute);//ActivatedRoute est un service qui permet de récupérer les informations de la route active
+  //ici on récupère l'identifiant du Pokémon à partir du paramètre de la route
+  readonly pokemonService = inject(PokemonService);//ici on injecte le service PokemonService qui permet de récupérer les données des Pokémons
+  readonly pokemonId = Number(this.route.snapshot.paramMap.get('id'));//Permet de récupérer l'identifiant du Pokémon à partir du paramètre de la route
+  readonly pokemon = signal(this.pokemonService.getPokemonById(this.pokemonId)).asReadonly();//Permet de récupérer le Pokémon correspondant à l'identifiant
 }
