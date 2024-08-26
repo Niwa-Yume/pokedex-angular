53556cb6a53b0b453f73348beeba51bf7e0b11f0
Julien Castro <julien.castro@protonmail.com>
2024-08-26 16:39:10 +0200

    VERSION FINAL

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,24 @@
-// src/app/app.config.ts
-import { ApplicationConfig } from '@angular/core';
-import { Routes, provideRouter } from '@angular/router';
+import { provideRouter, Routes } from '@angular/router';
 import { PokemonListComponent } from './pokemon/pokemon-list/pokemon-list.component';
 import { PokemonProfileComponent } from './pokemon/pokemon-profile/pokemon-profile.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ApplicationConfig } from '@angular/core';
 
 const routes: Routes = [
-  { path: 'pokemons/:id', component: PokemonProfileComponent }, // Permet de naviguer vers le profil d'un Pokémon en fonction de son identifiant
-  { path: 'pokemons', component: PokemonListComponent },
+  //CHEMIN POUR METTRE EN PLACE LES ROUTES POUR LES NOMS DE PAGE ET LES COMPOSANTS ASSOCIÉS
+  { 
+    path: 'pokemons', 
+    component: PokemonListComponent,
+    title: 'Pokédex' 
+  },
+  { 
+    path: 'pokemons/:id', 
+    component: PokemonProfileComponent, 
+    title: 'Pokémon',
+  },
+  //CHEMIN POUR LES LIENS DE NAVIGATION
   { path: '', redirectTo: '/pokemons', pathMatch: 'full' },
-  { path: '**', component: PageNotFoundComponent },
+  { path: '**', component: PageNotFoundComponent, title: 'Page introuvable' },
 ];
 
 export const appConfig: ApplicationConfig = { // Permet de configurer l'application Angular et appconfig: ApplicationConfig est un objet qui contient la configuration de l'application
